5de787290df4046302ddcd7b235476d00586f495
Claes 'Letharion' Gyllensvärd <letharion@gmail.com>
2014-12-17 10:52:35 +0100

    Correct grammar in comment for PHP 5.3 support

--- Letharion/Functional/Functional.php
+++ Letharion/Functional/Functional.php
@@ -43,7 +43,7 @@ class Functional {
   }
 
   public function execute($callback) {
-    // 5.3 isn't officially supported, but if it doesn't make more than this...
+    // 5.3 isn't officially supported, but if it doesn't require more than this...
     if (strpos(PHP_VERSION, '5.3') === 0 && is_array($callback)) {
       $callback[0]->$callback[1]($this);
     }
